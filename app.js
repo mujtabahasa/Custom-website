93ac60e21fcb9c8ca9f424e76f5139fd4e8b7a60
mujtaba <mujtabahasan441@gmail.com>
2023-06-14 07:23:00 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function  myFunc(){
+    let x = document.getElementById("nav-list");
+    if(x.style.display == "block"){
+        x.style.display = "none";
+    }else{
+        x.style.display = "block";
+    }
+}
